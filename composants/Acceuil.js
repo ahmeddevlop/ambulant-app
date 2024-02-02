91bb3f18d76e8cf63c53568b06ab4dd065e93e73
ahmeddevlop <ahmeddevlop@gmail.com>
2024-02-02 12:04:50 +0400

    version avec choisir prix

--- composants/Acceuil.js
+++ composants/Acceuil.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 const Acceuil = ({ navigation }) => {
   return (
     <View style={{ flex: 1, display: "flex" }}>
       <View style={style.titreView}>
-        <Text style={style.titreText}>Bienvenue dans Application Ambulant</Text>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../composants/Image/acceuil.png")}
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: "contain",
+              margin: 30,
+            }}
+          />
+        </View>
       </View>
 
       <View style={style.btnView}>
@@ -50,7 +60,9 @@ const style = StyleSheet.create({
     marginBottom: 5,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "green",
+    backgroundColor: "#07d934",
+    width: "70%",
+    borderRadius: 10,
   },
   btnText: { fontSize: 15, fontWeight: "bold", color: "snow" },
 });
